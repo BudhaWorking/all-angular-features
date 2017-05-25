9886e7693d858011f3c22116a0d8e526dcbed721
BudhaWorking <abhicse93@gmail.com>
2017-05-25 21:55:57 +0530

    this is my custom directive example

---
+++ directive/directive.js
@@ -0,0 +1,11 @@
+app.directive('myProductDirective',function(){
+
+	return {
+		restrict: 'EA',
+		 //template: '<h1>This is first message<h1>',
+		// templateUrl: 'my-info-message.html',
+		//scope: true,
+		
+		templateUrl: 'directive/view.html'   // run in fire fox
+		}
+});
